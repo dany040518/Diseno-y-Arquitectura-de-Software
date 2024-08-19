199e0f775a53bd56d5318741b8987056c5304c27
dany040518 <140460227+dany040518@users.noreply.github.com>
2024-08-19 18:19:23 -0500

    Delete Taller-2/DYAS-GuasappMessenger directory

--- Taller-2/DYAS-GuasappMessenger/src/main/java/main/java/edu/unisabana/dyas/patterns/util/MessageSender.java
+++
@@ -1,5 +0,0 @@
-package main.java.edu.unisabana.dyas.patterns.util;
-
-public interface MessageSender {
-    void sendMessage(String message);
-}
--- Taller-2/DYAS-GuasappMessenger/src/main/java/main/java/edu/unisabana/dyas/patterns/util/MessagingClient.java
+++
@@ -1,9 +0,0 @@
-package main.java.edu.unisabana.dyas.patterns.util;
-
-// Clase original proporcionada por terceros
-public class MessagingClient implements MessageSender {
-    
-    public void sendMessage(String message) {
-        System.out.println("Sending message: " + message);
-    }
-}
